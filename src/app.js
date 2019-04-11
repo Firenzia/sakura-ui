decbf186f6c0d83c16b21880131552c3656d0082
LEXINFINTECH\aislingxie <aislingxie@lexinfintech.com>
2019-04-11 11:54:15 +0800

    parcel打包

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import  Vue from 'vue'
+import button from './button'
+
+Vue.component('s-button', button)
+
+new Vue({
+    el:'#app'
+})
---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="s-button">122</button>
+</template>
+<script>
+export default {
+    name:'s-button'
+}
+</script>
+<style lang="scss" scoped>
+ .s-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+
+</style>
+
+
