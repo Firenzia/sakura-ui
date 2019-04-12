4c5994af81421d16ed64f1e5805b96a28471b1d9
LEXINFINTECH\aislingxie <aislingxie@lexinfintech.com>
2019-04-12 16:50:51 +0800

    增加btnGroup和使用校验

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import  Vue from 'vue'
 import button from './button'
 import icon from './icon'
+import buttonGroup from './button-group'
 
+Vue.component('s-button-group', buttonGroup)
 Vue.component('s-button', button)
 Vue.component('s-icon', icon)
+
 
 new Vue({
     el:'#app',
---
+++ src/button-group.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="s-btn-group">
+       <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+   name:'s-button-group',
+   mounted(){
+       for(let node of this.$el.children){
+          if(node.nodeName.toLowerCase() !== 'button'){
+            console.warm('使用不规范，请参考官方文档')
+          }
+       }
+   }
+}
+</script>
+<style lang="scss" scoped>
+    .s-btn-group{
+        display: inline-flex;
+        vertical-align: middle;
+        > .s-button{
+          border-radius:0;
+          &:not(:first-child){
+              margin-left:-1px;
+          }
+          &:first-child{
+            border-top-left-radius: var(--border-radius);
+            border-bottom-left-radius: var(--border-radius);
+          }
+          &:last-child{
+           border-top-right-radius: var(--border-radius);
+           border-bottom-right-radius: var(--border-radius);
+          }
+          &:hover{
+            position: relative;
+            z-index: 1;
+          }
+        }
+        
+    }
+</style>
+
