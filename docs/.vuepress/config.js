a8047d96f2358085abfb9f4072fe138d8bddd273
si <249428629@qq.com>
2019-05-04 22:54:14 +0800

    favicon 问题 :cherry_blossom:

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,32 +2,35 @@ module.exports = {
     base:'/sakura-ui/',
     title: 'Sakura UI',
     description: 'Inspiration from heian sakura',
+    head: [
+      ['link', { rel: 'icon', href: './favicon.png' }]
+   ],
     themeConfig: {
-        nav: [
-          { text: 'Home', link: '/' },
-          // { text: 'Guide', link: '/guide/' },
-          { text: 'Github', link: 'https://github.com/Firenzia/sakura-ui/' },
-        ],
-        sidebar: [
+      nav: [
+        { text: 'Home', link: '/' },
+        // { text: 'Guide', link: '/guide/' },
+        { text: 'Github', link: 'https://github.com/Firenzia/sakura-ui/' },
+      ],
+      sidebar: [
+          {
+              title: '开发指南',
+              collapsable: true,
+              children: [
+                'guide/install.md',
+                'guide/get-started.md'
+              ]
+            },
             {
-                title: '开发指南',
-                collapsable: true,
-                children: [
-                  'guide/install.md',
-                  'guide/get-started.md'
-                ]
-              },
-              {
-                title: '组件',
-                collapsable: true,
-                children: [
-                  'components/basic/',
-                  'components/form/',
-                  'components/navigation/',
-                  'components/notice/',
-                  'components/other/'
-                ]
-              },
-          ]
+              title: '组件',
+              collapsable: true,
+              children: [
+                'components/basic/',
+                'components/form/',
+                'components/navigation/',
+                'components/notice/',
+                'components/other/'
+              ]
+            },
+        ]
       }
   }
